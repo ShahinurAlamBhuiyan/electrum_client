fcb88e4e22d24b89df778e07656dc91d9419c98b
FahadAlHossain <fahadalhossain5@gmail.com>
2024-04-01 21:06:41 +0600

    videos card and details add

--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -1,11 +1,13 @@
 import CourseBanner from "../../components/Courses/CourseBanner"
 import CourseContainer from  "../../components/Courses/CourseContainer"
+import VideosContainer from "../../components/Courses/VideosContainer"
 
 const Courses = () => {
   return (
     <div>
       <CourseBanner/>
       <CourseContainer/>
+      <VideosContainer/>
     </div>
   )
 }
