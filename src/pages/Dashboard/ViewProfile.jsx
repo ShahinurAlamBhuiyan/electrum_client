f305ca115ad00dd5fc8077236aeb4def64fa15d3
Amit Mahmud Sabbir <amitsclan26@gmail.com>
2024-05-07 00:30:39 +0600

    dashboard edited

--- src/pages/Dashboard/ViewProfile.jsx
+++ src/pages/Dashboard/ViewProfile.jsx
@@ -5,11 +5,22 @@ const ViewProfile = () => {
 
 
   return (
-    <div>
-      <h2>Profile</h2>
-      <p>Hello {loggedInUserInfo.name}, Welcome to Electrum Dashboard</p>
-      <p>Email: {loggedInUserInfo.email}</p>
-    </div>
+    // <div>
+    //   <h2>Profile</h2>
+    //   <p>Hello {loggedInUserInfo.name}, Welcome to Electrum Dashboard</p>
+    //   <p>Email: {loggedInUserInfo.email}</p>
+    //   <p>Role: {loggedInUserInfo.role}</p>
+    //   {/* <p>ID: {loggedInUserInfo._id}</p> */}
+      
+    // </div>
+    <div className="profile-container">
+            <h2 className="profile-heading">Profile</h2>
+            <div className="profile-info">
+                <p className="welcome-message">Hello, <span className="user-name">{loggedInUserInfo.name}</span></p>
+                <p className="profile-detail"><strong>Email:</strong> {loggedInUserInfo.email}</p>
+                <p className="profile-detail"><strong>Role:</strong> {loggedInUserInfo.role}</p>
+            </div>
+        </div>
   )
 }
 
