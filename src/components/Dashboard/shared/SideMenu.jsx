fe4b7964a61a0f428bbb76050ce87660cb3e7a82
Amit Mahmud Sabbir <amitsclan26@gmail.com>
2024-05-07 02:47:09 +0600

    updated dashboard sidebar

--- src/components/Dashboard/shared/SideMenu.jsx
+++ src/components/Dashboard/shared/SideMenu.jsx
@@ -4,15 +4,17 @@ import './Style.css'
 const SideMenu = () => {
   const navigate = useNavigate()
   return (
-    <div className='sidebar_container'>
-      <h1 style={{ cursor: 'pointer' }} onClick={() => navigate('/')}>
-        Electrum
-      </h1>
-      <a href='/dashboard/profile'>profile</a> <br />
-      <a href='/dashboard/order'>order</a> <br />
-      <a href='/dashboard/add-component'>add component</a> <br />
-      <a href='/dashboard/my-components'>my component</a> <br />
-    </div>
+    <div className="sidebar-container">
+            <h1 className="logo" onClick={() => navigate('/')}>
+                Electrum
+            </h1>
+            <div className="nav-links">
+                <a href="/dashboard/profile">Profile</a>
+                <a href="/dashboard/order">Order</a>
+                <a href="/dashboard/add-component">Add Component</a>
+                <a href='/dashboard/my-components'>My Component</a> <br />
+            </div>
+        </div>
   )
 }
 
